369fd4dd16b853b7b125c4e2b9bf9c08be74d541
Alviam564 <alvarezwilliam543@gmail.com>
2025-05-10 19:01:45 -0400

    mini change

--- src/components/FooterU.jsx
+++ src/components/FooterU.jsx
@@ -9,7 +9,7 @@ const FooterU = () => {
             <div className="background-wrapperO">
                 <div className="row footer__row">
                     <button onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
-                        <img className="overlay-img main-page-logo-d main-page-logo-mini" src={ PTCGL } alt="pokemon TCG Logo"/>
+                        <img className="overlay-img main-page-logo main-page-logo-mini-2" src={ PTCGL } alt="pokemon TCG Logo"/>
                     </button>
                     <img className="bg-imageO" src={ CRWC } alt="Charizard & Reshiram" />
                 </div>
