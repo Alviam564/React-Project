3e724c49e5b78761972e88a42c849f5e2fd5c4cc
Alviam564 <alvarezwilliam543@gmail.com>
2025-05-15 20:32:20 -0400

    update

--- src/App.js
+++ src/App.js
@@ -6,17 +6,16 @@ import Search from './pages/Search';
 import PreloaderGate from './components/PreloaderGate';
 import CardFinder from './components/CardFinder';
 import './styles.css';
-/* remember alviam546/PokemonTCG */
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home /> } />
-        <Route path="/teamup" element={<PreloaderGate setid="sm9"><TeamUp /></PreloaderGate>} />
-        <Route path="/unbrokenbonds" element={<PreloaderGate setid="sm10"><UnbrokenBond /></PreloaderGate>} />
-        <Route path="/card/:id" element={<CardFinder />} />
-        <Route path="/search/:searchTerm" element={<Search />} /> 
+        <Route path="alviam564/PokemonUTVUB/" element={<Home /> } />
+        <Route path="alviam564/PokemonUTVUB/teamup" element={<PreloaderGate setid="sm9"><TeamUp /></PreloaderGate>} />
+        <Route path="alviam564/PokemonUTVUB/unbrokenbonds" element={<PreloaderGate setid="sm10"><UnbrokenBond /></PreloaderGate>} />
+        <Route path="alviam564/PokemonUTVUB/card/:id" element={<CardFinder />} />
+        <Route path="alviam564/PokemonUTVUB/search/:searchTerm" element={<Search />} /> 
       </Routes>
     </Router>
   );
